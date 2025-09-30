4f42e4e622837d636cdb54d3032411d31996332c
s202245800 <myalsuhaibani@gmail.com>
2025-09-30 12:16:58 +0300

    first trial code

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-
+import StudentCard from './components/StudentCard';
 function App() {
   return (
     <div className="app">
@@ -10,7 +10,13 @@ function App() {
 
       <main className="dashboard-main">
         <div className="cards-container">
-          {/* TODO: Import and render StudentCard components here */}
+          {/* TODO: Import and render StudentCard components here */
+                <div>
+                         <h1>Student Info</h1>
+                         <StudentCard name="Mohammed" id="20223456" dept="ICS" />
+                         <StudentCard name="Ahemad" id="20225678" dept="ICS" />
+                       </div>
+            }
         </div>
       </main>
     </div>
